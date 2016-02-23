4b0500eaf34a856c164b0dbbcf6b6302a992b919
Alex Vogel <avogelm@gmail.com>
2016-02-23 22:56:18 +0100

    plantilla de la prueba de api restful

---
+++ api.php
@@ -0,0 +1,15 @@
+<?php
+  if(isset($_GET[''])){
+
+  }else if(isset($_POST[''])){
+
+  }else if(isset($_PUT[''])){
+
+  }else if(isset($_DELETE[''])){
+
+  }else if(isset($_PATCH[''])){
+
+  }else{
+
+  }
+?>
